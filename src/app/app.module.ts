169fbdcf31def599c6ae18e66bdfbef5641a24fc
Sachin Choudhary <sachin.choudhary58@gmail.com>
2020-01-30 22:06:28 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { IntroComponent } from './intro/intro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    IntroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
