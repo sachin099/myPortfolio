8285df636a615ebd578b5ed874cafbd77f75de7d
Sachin Choudhary <sachin.choudhary58@gmail.com>
2020-02-13 01:35:44 +0530

    Auto-generated commit

---
+++ app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { IntroComponent } from './intro/intro.component';
+import { BioComponent } from './bio/bio.component';
+import { TechStackComponent } from './tech-stack/tech-stack.component';
+import { NgwWowModule } from 'ngx-wow';
+import { ExperienceComponent } from './experience/experience.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    IntroComponent,
+    BioComponent,
+    TechStackComponent,
+    ExperienceComponent,
+    SocialMediaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgwWowModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
